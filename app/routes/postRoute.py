18c3a3a0f628a2d824077a2f197878a3a8a19731
manuemedina <manuel.mmedina@mercadolivre.com>
2026-01-13 21:57:04 -0500

    .

---
+++ app/routes/postRoute.py
@@ -0,0 +1,10 @@
+from schemas.postSchema import PostCreate
+from fastapi import APIRouter, Depends, HTTPException
+
+
+router = APIRouter()
+
+@router.post("/posts")
+async def create_post(post: PostCreate):
+    return {"message": "Post created successfully"}
+    
