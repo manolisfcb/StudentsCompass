aa581648d47ec61f751052f1c09b3b3418b05459
manolisfcb <79519842+manolisfcb@users.noreply.github.com>
2026-06-17 18:51:15 -0400

    chore: remove unused service stubs

--- app/services/images.py
+++
@@ -1,7 +0,0 @@
-from fastapi import UploadFile
-
-from app.services.mediaStorageService import MediaUploadResult, get_media_storage_service
-
-
-def upload_media(file: UploadFile, file_name: str, folder: str = "images/") -> MediaUploadResult:
-    return get_media_storage_service().upload_media(file=file, file_name=file_name, folder=folder)
